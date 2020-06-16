730baa620ce3124119c52ae76e834560583a636e
Alexandre Lima <lima@fatecsp.br>
2020-06-16 18:43:45 -0300

    Finalizando a fase 3 - faltam testes e extrair os métodos de busca para classes

--- db/connect.rb
+++ db/connect.rb
@@ -1,10 +1,2 @@
 require 'sequel'
 DB = Sequel.connect('sqlite://censo.db') # requires sqlite3
-
-#top 10
-=begin
-SELECT code, name, population
-FROM counties
-ORDER BY population
-LIMIT 10
-=end
--- lib/cli.rb
+++ lib/cli.rb
@@ -22,6 +22,8 @@ class CLI
         CLI.list_counties
       when "busca"
         CLI.codigo
+      when "buscauf"
+        CLI.codigouf
       when "self help"
         puts "Thank you for taking the time to reflect, and recognizing the importance of self care. <3"
         puts "\n"
@@ -45,7 +47,8 @@ class CLI
     puts "  top.10\t\t:lista os 10 municípios mais populosos".green
     puts "  todos\t\t\t:lista todos os municípios".green
     puts "Data".bold
-    puts "  busca\t\t\t:buscar por codigo ou nome".green
+    puts "  busca\t\t\t:buscar por codigo ou nome do município".green
+    puts "  buscauf\t\t\t:buscar por codigo ou nome do estado".green
     puts "Sair".bold
     puts "  sair\t\t\t:sair do programa".green
     puts "  exit\t\t\t:também sai do programa".green
@@ -65,19 +68,28 @@ class CLI
     puts "Help".bold
     puts "  help\t\t\t:mostrar esse menu ajuda(help)".green
     puts "Listar".bold
-    puts "  list\t\t\t:lista tudo #{menu}s".green
+    puts "  todos\t\t\t:lista tudo #{menu}s".green
     puts "Navegação".bold
     puts "  exit\t\t\t:sair para o menu principal".green
     puts "  menu\t\t\t:mesma coisa que exit".green
   end
 
-  def self.list_counties #list_countries
+  def self.list_counties 
     DB['SELECT code, name, population 
       FROM counties
       ORDER BY population DESC'].each do |county|
-    puts "Código: "+ "#{county[:code]}".green + +
+    puts "Código: "+ "#{county[:code]}".green + 
          " - Município: "+ "#{county[:name]}".green + 
          " - Populaçao: "+ "#{county[:population]}".green
+    end
+  end
+
+  def self.list_states 
+    DB['SELECT code, name, population 
+      FROM states
+      ORDER BY code DESC'].each do |state|
+    puts "Código: "+ "#{state[:code]}".green + 
+         " - Estado: "+ "#{state[:name]}".green
     end
   end
 
@@ -86,6 +98,55 @@ class CLI
       puts "Código: "+ "#{county[:code]}".green + 
            " - Município: "+ "#{county[:name]}".green + 
            " - Populaçao: "+ "#{county[:population]}".green
+    end
+  end
+
+  def self.state_data(counties)
+    soma = counties.sum(:population)
+    media = counties.avg(:population)
+    puts "Soma: "+ "#{soma.round(2)}".green
+    puts "Média da população dos municípios: "+ "#{media.round(2)}".green
+  end
+
+  def self.codigouf
+    loop do
+      puts "\nDigite 'listar' para listar todos os estados,
+            ou 'sair' para retornar ao menu principal".magenta
+      print "Digite um código ou nome do estado: ".cyan
+
+      input = gets.chomp
+
+      break if input == "exit" || input == "menu"
+
+      if input == "todos"
+        CLI.list_states
+      elsif input == "help"
+        CLI.submenu_help("viewer")
+      else
+        begin
+          if input =~ /\d/ 
+            id = input.to_i
+            state_code = DB['SELECT code FROM states WHERE code = ?', id].get(:code)
+            puts state_code
+            counties = DB[:counties].where(Sequel.like(:code, "#{state_code}%"))
+            raise CLIError if counties.empty?
+            CLI.county_data(counties.reverse_order(:population).limit(10))
+            CLI.state_data(counties)
+            puts "\n"
+          else
+            name = input
+            state_code = DB[:states].where(Sequel.like(:name, "%#{name}%")).get(:code)
+            counties = DB[:counties].where(Sequel.like(:code, "#{state_code}%"))
+            raise CLIError if counties.empty?
+            CLI.county_data(counties.reverse_order(:population).limit(10))
+            CLI.state_data(counties)
+            puts "\n"
+          end
+          ## Error handling
+          rescue CLIError => error
+            puts error.message
+          end
+      end
     end
   end
 
